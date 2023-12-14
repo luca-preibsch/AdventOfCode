f88a686c50f9df672cf188d508dfffcdee280795
Luca Preibsch <luca.preibsch@fau.de>
2023-12-14 22:27:26 +0100

    solve ac23 ex2.2

--- src/aoc23/day2/a2.kt
+++ src/aoc23/day2/a2.kt
@@ -1,24 +1,25 @@
 package aoc23.day2
 
-fun main() {
-    val cubeCounts = mapOf(
-        "red" to 12,
-        "green" to 13,
-        "blue" to 14
-    )
+import kotlin.math.max
 
+fun main() {
     var sum = 0
     generateSequence(::readLine).forEach gameLoop@{ game ->
+        val maxCubes = mutableMapOf(
+            "red" to 0,
+            "green" to 0,
+            "blue" to 0
+        )
+
         val (gameHead, gameContent) = game.split(':')
         val gameNumber = gameHead.split(' ').last().toInt()
         gameContent.split(';').forEach { round ->
             round.split(',').forEach {
                 val (count, color) = it.trim(' ').split(' ')
-                if (count.toInt() > cubeCounts[color]!!)
-                    return@gameLoop
+                maxCubes[color] = max(maxCubes[color]!!, count.toInt())
             }
         }
-        sum += gameNumber
+        sum += maxCubes.values.reduceOrNull(Int::times) ?: 0
     }
     println(sum)
 }
